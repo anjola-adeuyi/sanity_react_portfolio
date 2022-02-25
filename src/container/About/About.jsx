62490189270a266bad80c155da26e2f881729646
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-02-25 01:37:06 +0100

    Higher Order Component and shared Functionalities

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { client, urlFor } from '../../client';
+import { AppWrap } from '../../wrapper';
 import './About.scss';
 
 const About = () => {
@@ -43,4 +44,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
